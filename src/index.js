2009b26b9921c587cbae62274c01883f4b132348
phumlajobe <phumlasj@gmail.com>
2024-03-13 12:34:05 +0200

    Added date for searched city

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
+function formatDate(date){
+
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let time = `${hour}:${minutes}`
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    let day = days[date.getDay()];
+    return `${day} ${time}`;
+}
+
 function findTemperature(response){
 
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.city;
+
     let temperatureElement = document.querySelector("#temparature-value");
     let temparature = response.data.temperature.current;
     temperatureElement.innerHTML = Math.round(temparature);
-    let tempIcon = document.querySelector("#temparature-icon");
-    console.log(response.data);
+    
+    let tempIcon = document.querySelector(".icon");
+    let iconUrl = response.data.condition.icon_url;
+    tempIcon.innerHTML = `<img src="${iconUrl}" 
+    alt="Temperature Icon" id="temparature-icon">`
+
+    let date = new Date(response.data.time * 1000);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(date);
+    
 
     let humidityElement = document.querySelector("#humidity");
     let windElemnt = document.querySelector("#wind");
+    let skyElement = document.querySelector("#sky");
     let wind = Math.round(response.data.temperature.humidity);
     humidityElement.innerHTML = `${wind}%`;
     let speed = Math.round(response.data.wind.speed);
     windElemnt.innerHTML = `${speed}km/h`;
-    
+    skyElement.innerHTML = response.data.condition.description;
 
 }
 
@@ -36,3 +57,4 @@ function searchCity(event){
 let searchElement = document.querySelector(".search-form");
 
 searchElement.addEventListener("submit",searchCity);
+findCity("Johannesburg");
